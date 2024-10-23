238f722c7fbd54bce65e9829d7627363e0b5bd6c
Nosirsho <noisershoh@gmail.com>
2024-10-23 16:18:30 +0500

    Redirect StudentTable

--- src/components/Tables/StudentTable/StudentTable.vue
+++ src/components/Tables/StudentTable/StudentTable.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { provide, onMounted, ref } from 'vue'
 import axios from 'axios'
+import { useAutoAnimate } from '@formkit/auto-animate/vue'
 
 import StudentTableItem from './StudentTableItem.vue'
 import StudentEditForm from '../../Forms/StudentEditForm.vue'
+
+const [parent] = useAutoAnimate()
 
 const items = ref([])
 const isVisibleForm = ref(false)
 const editStudentId = ref(null)
 const isEdit = ref(false)
+const searchInput = ref()
 
 const handleAddStudent = (student) => {
   items.value.push(student)
@@ -25,6 +29,10 @@ const handleStudentEdit = (studentId) => {
   editStudentId.value = studentId
   isEdit.value = true
 }
+const handleDeleteStudent = (studentId) => {
+  items.value = items.value.filter((t) => t.id !== studentId)
+}
+
 const openAddForm = async () => {
   isVisibleForm.value = true
   isEdit.value = false
@@ -38,6 +46,19 @@ provide('AddFormActions', {
   closeAddForm
 })
 
+const searchStudents = async () => {
+  try {
+    await axios
+      .get('http://localhost:5296/Student/search', { params: { search: searchInput.value } })
+      .then((response) => {
+        items.value = response.data
+        console.log(response.data)
+      })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 onMounted(async () => {
   try {
     const { data } = await axios.get('http://localhost:5296/Student')
@@ -48,7 +69,7 @@ onMounted(async () => {
 })
 </script>
 <template>
-  <div>
+  <div ref="parent">
     <StudentEditForm
       @close-add-form="closeAddForm"
       @addStudent="handleAddStudent"
@@ -57,6 +78,49 @@ onMounted(async () => {
       :isEdit="isEdit"
       v-if="isVisibleForm"
     />
+    <!--Search Input Start-->
+    <div class="max-auto mx-auto">
+      <label
+        for="default-search"
+        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >Search</label
+      >
+      <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg
+            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          v-model="searchInput"
+          type="search"
+          id="default-search"
+          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder='Поиск по шаблону "Фамилия Имя Отчество"'
+          required
+        />
+        <button
+          @click="searchStudents"
+          class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+    <!--Search Input End-->
+
     <table class="min-w-full">
       <thead>
         <tr>
@@ -93,7 +157,7 @@ onMounted(async () => {
         </tr>
       </thead>
 
-      <tbody class="bg-white">
+      <tbody class="bg-white" v-auto-animate>
         <!--StudentTableItem-->
         <StudentTableItem
           v-for="(item, index) in items"
@@ -104,6 +168,7 @@ onMounted(async () => {
           :grade-level="item.gradeLevel"
           :sex="item.sex"
           @editStudent="handleStudentEdit"
+          @deleteStudent="handleDeleteStudent"
         />
         <!--End StudentTableItem-->
       </tbody>
--- src/components/Tables/StudentTable/StudentTableItem.vue
+++ src/components/Tables/StudentTable/StudentTableItem.vue
@@ -1,11 +1,24 @@
 <script setup>
-defineProps({
+import axios from 'axios'
+
+const props = defineProps({
   id: String,
   fullName: String,
   birthDate: String,
   gradeLevel: String,
   sex: String
 })
+
+const deleteStudent = async () => {
+  try {
+    await axios.delete(`http://localhost:5296/Student/${props.id}`).then((response) => {
+      emit('deleteStudent', response.data)
+      console.log(response.data)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
 </script>
 <template>
   <tr class="hover:bg-slate-100">
@@ -32,8 +45,18 @@ defineProps({
     <td
       class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200"
     >
-      <button class="text-indigo-600 hover:text-indigo-900" @click="$emit('editStudent', id)">
+      <button
+        class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-md text-sm px-3 pb-0.5 dark:focus:ring-yellow-900"
+        @click="$emit('editStudent', id)"
+      >
         Edit
+      </button>
+      |
+      <button
+        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-md text-sm px-3 pb-0.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        @click="deleteStudent"
+      >
+        Delete
       </button>
     </td>
   </tr>
